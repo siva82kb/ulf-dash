Saved filetimes to prevent reading of already analysed files. Fixed bugs with average measure calculation.

tempsupport.py
@@ -10,8 +10,10 @@ import pandas
 from pandas import DataFrame
 from pandas import Timestamp, Timedelta
 from datetime import datetime as dt
+from scipy import signal
 import datetime
 import numpy as np
+
 
 from monalysa import misc
 
@@ -148,6 +150,61 @@ class ARIMU(object):
     
     def __repr__(self):
         return f"ActiGraphData(filename='{self._filename}', id='{self._id}')"
+
+
+def from_gmac(acc_forearm, acc_ortho1, acc_ortho2, sampfreq, pitch_threshold=30, counts_threshold=0):
+    """
+    Computes UL use using the GMAC algorithm with pitch and counts estimated only from acceleration.
+    Args:
+        acc_forearm (np.array):  1D numpy array containing acceleration along the length of the forearm.
+        acc_ortho1 (np.array): 1D numpy array containing acceleration along one of the orthogonal axis to the forearm.
+        acc_ortho2 (np.array): 1D numpy array containing acceleration along the other orthogonal axis to the forearm.
+        sampfreq (int): Sampling frequency of acceleration data.
+        pitch_threshold (int): Pitch between +/- pitch_threshold are considered functional, default=30 (Leuenberger et al. 2017).
+        counts_threshold (int): Counts greater than counts_threshold are considered functional, default=0 (optimized for youden index).
+
+    Returns:
+        tuple[np.array, np.array]: A tuple of 1D numpy arrays. The first 1D
+        array is the list of time indices of the computed UL use signal. The
+        second ID array is the UL use signal, which is a binary
+        signal indicating the presence or absence of a "functional"
+        movement any time instant.
+    """
+    assert len(acc_forearm) == len(acc_ortho1), "acc_forearm, acc_ortho1 and acc_ortho2 must be of equal length"
+    assert len(acc_ortho1) == len(acc_ortho2), "acc_forearm, acc_ortho1 and acc_ortho2 must be of equal length"
+    assert sampfreq > 0, "sampfreq must be a positive integer"
+
+    # 1 second moving average filter
+    acc_forearm = np.append(np.ones(sampfreq - 1) * acc_forearm[0], acc_forearm)  # padded at the beginning with the first value
+    acc_forearm = np.convolve(acc_forearm, np.ones(sampfreq), 'valid') / sampfreq
+    acc_forearm[acc_forearm < -1] = -1
+    acc_forearm[acc_forearm > 1] = 1
+
+    pitch_hat = -np.rad2deg(np.arccos(acc_forearm))
+    # pitch_hat = -np.rad2deg(np.arccos(acc_forearm)) + 90
+
+    hpf_cutoff = 1  # 1Hz high pass filter
+    hfir = signal.firwin(25, hpf_cutoff, fs=sampfreq, pass_zero='highpass')
+    acc_forearm_filt = signal.filtfilt(hfir, 1, acc_forearm)
+    acc_ortho1_filt = signal.filtfilt(hfir, 1, acc_ortho1)
+    acc_ortho2_filt = signal.filtfilt(hfir, 1, acc_ortho2)
+
+    deadband_threshold = 0.068  # Brond et al. 2017
+    acc_forearm_filt[np.abs(acc_forearm_filt) < deadband_threshold] = 0
+    acc_ortho1_filt[np.abs(acc_ortho1_filt) < deadband_threshold] = 0
+    acc_ortho2_filt[np.abs(acc_ortho2_filt) < deadband_threshold] = 0
+
+    amag = [np.linalg.norm(x) for x in np.column_stack((acc_forearm_filt, acc_ortho1_filt, acc_ortho2_filt))]
+    amag = [sum(amag[i:i + sampfreq]) for i in range(0, len(amag), sampfreq)]
+
+    # 5 second moving average filter
+    window = 5  # Bailey et al. 2014
+    amag = np.append(np.ones(window - 1) * amag[0], amag)
+    amag = np.convolve(amag, np.ones(window), 'valid') / window
+
+    _uluse = [1 if np.abs(pitch) < pitch_threshold and count > counts_threshold else 0
+                for pitch, count in zip(pitch_hat[0:len(pitch_hat):sampfreq], amag)]
+    return (np.arange(len(_uluse)), _uluse)
     
 
 def grav_sub_mag(acclmag: np.array, usesig: np.array,
@@ -177,5 +234,5 @@ def grav_sub_mag(acclmag: np.array, usesig: np.array,
     
     _n_acclmag = len(acclmag)
     _xval = np.arange(0, len(acclmag), nsample)
-    _yval = np.mean((acclmag - 1).reshape(_n_acclmag // nsample, nsample), axis=1) * usesig
+    _yval = np.mean(np.abs(acclmag - 1).reshape(_n_acclmag // nsample, nsample), axis=1) * usesig
     return (_xval, _yval)